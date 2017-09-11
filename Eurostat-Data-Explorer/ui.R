25598ecdaa744a062cabe7e61749935da999bc4d
czescmateusz <mateusz.piotr.pawlowski@gmail.com>
2017-09-11 16:36:15 +0200

    minor

--- Eurostat-Data-Explorer/ui.R
+++ Eurostat-Data-Explorer/ui.R
@@ -9,13 +9,14 @@ library(shinydashboard)
 # Define UI for application that draws a histogram
 shinyUI(dashboardPage(#theme="bootstrap.css",
   dashboardHeader(title = "Eurostat Indicators"),
-  dashboardSidebar(sidebarMenuOutput("menu"), menuItem("Introduction"), menuItem("Demography"), 
+  dashboardSidebar(sidebarMenuOutput("menu"), menuItem("Introduction", tabName = "introduction", icon = icon("dashboard")), menuItem("Demography"), 
                    menuItem("General Economic Overview"), menuItem("Industry"),
                    menuItem("General Economic Overview"), menuItem("Unemployment"),
                    menuItem("Explore"), menuItem("About me")
   ),
-  dashboardBody(fluidRow(
-    valueBoxOutput("populationbox"),
-    valueBoxOutput("gdpbox")
+  dashboardBody(tabItems(
+    tabItem(tabName = "introduction",
+            valueBoxOutput("populationbox")
+    )
   ))))
 
