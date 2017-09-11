a519ad6a528f90475f56ffb6701a52043ccdb15c
czescmateusz <mateusz.piotr.pawlowski@gmail.com>
2017-09-11 16:04:39 +0200

    Using dashboardPage instead of fluidPage

--- Eurostat-Data-Explorer/ui.R
+++ Eurostat-Data-Explorer/ui.R
@@ -7,18 +7,15 @@ library(rcdimple)
 library(shinydashboard)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(theme="bootstrap.css",
-  titlePanel("Eurostat Data Explorer"),
-  navlistPanel(
-    "Header",
-    tabPanel("Introduction", valueBoxOutput("populationbox"), valueBoxOutput("gdpbox")),
-    tabPanel("Demography",  dimpleOutput('chart')), 
-    tabPanel("General Economic Overview"),
-    tabPanel('Industry'),
-    tabPanel("General Economic Overview"),
-    tabPanel('Unemployment', dataTableOutput('tabUnemployment')),
-    tabPanel('Explore Eurostat database', textInput("query", "Search Eurostat Database", "Type in your query (ex. unemployment, GDP)"), 
-             dataTableOutput('tabEuroStat'))
-  )))
-
+shinyUI(dashboardPage(#theme="bootstrap.css",
+  dashboardHeader(title = "Eurostat Indicators"),
+  dashboardSidebar(sidebarMenuOutput("menu"), menuItem("Introduction"), menuItem("Demography"), 
+                   menuItem("General Economic Overview"), menuItem("Industry"),
+                   menuItem("General Economic Overview"), menuItem("Unemployment"),
+                   menuItem("Explore"), menuItem("About me")
+  ),
+  dashboardBody(fluidRow(
+    valueBoxOutput("populationbox"),
+    valueBoxOutput("gdpbox")
+  ))))
 
--- testing-functions.R
+++ testing-functions.R
@@ -12,3 +12,27 @@ print(tabela)
 
 #1Introduction: KPI (liczba ludnosci, liczba bezrobotnych, GDP per capita) + lisa krajow z UE -> przejscie do KPI dla pojedynczych krajow
 #2Demography: 
+
+#
+#Valuebox - works only with dashboad layout?
+#change fluidpage to dashboard layout
+
+library(shiny)
+library(rcdimple)
+library(shinydashboard)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(theme="bootstrap.css",
+                  titlePanel("Eurostat Data Explorer"),
+                  navlistPanel(
+                    "Header",
+                    tabPanel("Introduction", valueBoxOutput("populationbox"), valueBoxOutput("gdpbox")),
+                    tabPanel("Demography",  dimpleOutput('chart')), 
+                    tabPanel("General Economic Overview"),
+                    tabPanel('Industry'),
+                    tabPanel("General Economic Overview"),
+                    tabPanel('Unemployment', dataTableOutput('tabUnemployment')),
+                    tabPanel('Explore Eurostat database', textInput("query", "Search Eurostat Database", "Type in your query (ex. unemployment, GDP)"), 
+                             dataTableOutput('tabEuroStat'))
+                  )))
+
